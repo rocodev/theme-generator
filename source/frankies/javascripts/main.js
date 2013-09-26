2f60db2474bac917833b3501b25ba4324a1157c1
Zhusee <zhusee2@gmail.com>
2013-09-26 17:20:25 +0800

    Javascript to toggle scroll shadow

--- source/frankies/javascripts/main.js
+++ source/frankies/javascripts/main.js
@@ -12,6 +12,33 @@ var initSiteSearch = function() {
       })
 };
 
+var paintSiteNavShadow = function() {
+  var $siteNavLinks = $('.site-nav-links'),
+      $siteNavShadow = $('.site-nav-shadow');
+
+  var navLinkWidth = $siteNavLinks.width(),
+      navLinkScrollWidth = $siteNavLinks[0].scrollWidth,
+      navLinkScrollLeft = $siteNavLinks[0].scrollLeft,
+      navLinkWidthDiff = navLinkScrollWidth - navLinkWidth;
+
+  // Reset state
+  $siteNavShadow.removeClass('left-shadow right-shadow');
+
+  if (navLinkScrollWidth > navLinkWidth) { //Need scrolling
+    if (navLinkScrollLeft < navLinkWidthDiff) { // Have things in right
+      $siteNavShadow.addClass('right-shadow');
+    }
+
+    if (navLinkScrollLeft > 0) { // Have things in left
+      $siteNavShadow.addClass('left-shadow');
+    }
+  }
+};
+
 $(function() {
   initSiteSearch();
+  paintSiteNavShadow();
+
+  $('.site-nav-links').on('scroll', paintSiteNavShadow);
+  $(window).on('resize', paintSiteNavShadow);
 });
